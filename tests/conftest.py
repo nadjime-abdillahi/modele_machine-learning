6ee44799bb62f20d65dccbd1c1a91eb4c7656a6d
nadjime-abdillahi <nadjimeabdillahi@gmail.com>
2026-09-19 08:53:08 +0300

    fix: déplacement de conftest.py dans tests/ pour la résolution des fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,11 @@
 import pytest
 from fastapi.testclient import TestClient
 
-from app.database import engine, Base, get_db
+from app.database import engine, Base
 from app.main import app
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_database():
-    # Crée les tables dans la base de test
     Base.metadata.create_all(bind=engine)
     yield
     Base.metadata.drop_all(bind=engine)
